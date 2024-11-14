ace3a6f10063b02d4b266187bdc8d2214e46e8c4
Trương Minh <154775327+FlashTM123@users.noreply.github.com>
2024-11-14 11:22:35 +0700

    Add files via upload

--- resources/views/Category/Categorylist.blade.php
+++ resources/views/Category/Categorylist.blade.php
@@ -9,7 +9,7 @@
     <div class="container mt-5">
         <div class="row">
             <div class="col-12">
-                <h3 class="text-center mb-4">Category</h3>
+                <h3 class="page-header text-center mb-4">Category</h3>
             </div>
         </div>
 
